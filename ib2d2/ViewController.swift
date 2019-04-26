ad601f6f0234e5837ee1258c2a8dcb97cfd929ad
TJ Balon <balon@tangoworldwide.net>
2019-04-26 16:12:42 -0400

    Initial; Skeleton Design

---
+++ ib2d2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ib2d2
+//
+//  Created by balon on 4/24/19.
+//  Copyright © 2019 TJ balon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ib2d2/settingsVC.swift
@@ -0,0 +1,38 @@
+//
+//  settingsVC.swift
+//  ib2d2
+//
+//  Created by balon on 4/26/19.
+//  Copyright © 2019 TJ balon. All rights reserved.
+//
+
+import UIKit
+
+class settingsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    /* Set the tab name + image */
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        // Initialize Tab Bar Item
+        tabBarItem = UITabBarItem(title: "Settings", image: UIImage(named: "icon-books"), tag: 1)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
