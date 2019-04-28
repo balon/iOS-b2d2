cb488805681f9a5986785bbd42ca326c57620c3d
TJ Balon <balon@tangoworldwide.net>
2019-04-28 17:27:36 -0400

    Account Settings Layout

---
+++ ib2d2/webVC.swift
@@ -0,0 +1,30 @@
+//
+//  webVC.swift
+//  ib2d2
+//
+//  Created by balon on 4/28/19.
+//  Copyright © 2019 TJ balon. All rights reserved.
+//
+
+import UIKit
+
+class webVC: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
